115122ca5a8b76d43e9c0022e25dbfc34fef15b6
apakr <megapuppypower@gmail.com>
2025-01-31 14:45:42 -0600

    lab3 stuff from in lab done

--- homework2.R
+++ homework2.R
@@ -7,3 +7,14 @@
 set.seed(385)
 exp_sample <- rexp(500, 1/4)
 
+mean(exp_sample)
+# 4.280625
+
+var(exp_sample)
+# 16.48086
+
+hist(exp_sample)
+# Add plot
+
+?dexp
+?pexp
---
+++ lab3.R
@@ -0,0 +1,61 @@
+setwd("~/Desktop/stat385/")
+
+## Question 1: Load the data
+
+load(file="Alldata.Rdata")
+
+## Find the average of the gene expressions of gene with IDENTIFIER="RFC2" as responses
+
+subsetGenes<-sapply(Alldata$originaldata[Alldata$originaldata[,2]=="RFC2",3:110], as.numeric)
+averageRFC2<-colMeans(subsetGenes)
+resp <- averageRFC2
+
+## Question 2: Create predictor variables: Beverage and Hours 
+
+samIDs<-names(averageRFC2)
+Beverages<-(samIDs%in%Alldata$trt1)*1+(samIDs%in%Alldata$trt2)*2+(samIDs%in%Alldata$trt3)*3+(samIDs%in%Alldata$trt4)*4
+hours<-(samIDs%in%Alldata$time_h0)*0+(samIDs%in%Alldata$time_h1)*1+(samIDs%in%Alldata$time_h2)*2+(samIDs%in%Alldata$time_h4)*4+(samIDs%in%Alldata$time_h12)*12
+Beverages <- factor(Beverages)
+
+## Question 3: Plot the curves: average gene expression versus hours by beverages (should have numbers that come as a reuslt of summary from lm)
+
+colvec<-NULL
+pchvec<-NULL
+ltyvec<-NULL
+plot(hours,resp,xlab="Hours",ylab="gene expression average",type="n")
+for (bever in 1:4)
+{
+  BeverHourAve<-tapply(resp[(Beverages==bever)],hours[(Beverages==bever)],mean)
+  points(c(0,1,2,4,12),BeverHourAve,col=bever,pch=bever)
+  lines(c(0,1,2,4,12),BeverHourAve,col=bever)
+  colvec<-c(colvec,bever)
+  pchvec<-c(pchvec,bever)
+  ltyvec<-c(ltyvec,bever)
+}
+legendtext<-c("Alcohol","Grape juice","Red wine","Water")
+legend(8,4.5,legendtext,col=colvec,pch=pchvec,lty=ltyvec)
+
+## Question 4: Construct linear model for gene expression (should have an LM in it) (4 - model, 5 - coefficients, 6 - model, 7 - prediction)
+lm1 <- lm(averageRFC2 ~ Beverages)
+
+
+## Question 5: Analyze coefficient(s)
+summary(lm1)
+# Least squares estimate of different effect of alcohol and water on average gene expression of RFC2 is 0.02851, 
+# but since the p-value is not significant, this difference that we observe is not significantly different from 0.
+
+## Question 6: (should have an LM in it, call it lm3 so that it works in Q7)
+
+## Question 7: Define new data set for prediction
+
+newdata<-as.data.frame(list(BeverFac="1",hours=6, hours2=36, hours3=216))
+newprediction<-predict(lm3,newdata,interval="predict")
+
+
+
+
+
+
+
+
+

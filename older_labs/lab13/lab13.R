effe70c299865ca5b00aa2fdaa7713179fcca549
apakr <megapuppypower@gmail.com>
2025-04-28 13:21:56 -0500

    finished lab 13

--- older_labs/lab13/lab13.R
+++ older_labs/lab13/lab13.R
@@ -89,8 +89,29 @@ kmeans_vs_true
 # 1  1 18
 # 2 26  2
 
-# The k-means clustering with k=2 does not perform very well. Cluster 1 contains mostly BLC patients,
-# while Cluster 2 contains mostly non-BLC patients, but there is substantial mixing between the classes.
+# The k-means clustering with k=2 performs fine, but the groups are switched. Cluster 1 contains mostly BLC patients,
+# while Cluster 2 contains mostly non-BLC patients.
+
+############################################################################
+#
+# Additional: K-means clustering with k=3 and evaluate performance
+#
+############################################################################
+
+kmeans.cluster3<-kmeans(microdata2,3)
+plot(microdata2, col=kmeans.cluster3$cluster, main="K-means clustering (k=3)")
+points(kmeans.cluster3$centers,cex=2,pch=20, col=c(1:3))
+
+# Evaluate K-means clustering performance with k=3
+kmeans3_vs_true <- table(kmeans.cluster3$cluster, BLClabels)
+kmeans3_vs_true
+#   BLClabels
+#    1  2
+# 1 11  0
+# 2 15  2
+# 3  1 18
+
+# Looks a arguably better if we use an elbow of 3.
 
 ############################################################################
 #
@@ -121,6 +142,27 @@ hierarchical_vs_true
 # The hierarchical clustering using Manhattan distance and centroid linkage performs well. Cluster 1
 # groups mostly non-BLC patients, and Cluster 2 groups mostly BLC patients, with very little misclassification
 # compared to k-means. This method works better for our data.
+
+############################################################################
+#
+# Additional: Hierarchical clustering with k=3 and evaluate performance
+#
+############################################################################
+
+cuttree3 <- cutree(hclustCen, k=3)
+plot(hclustCen)
+rect.hclust(hclustCen, k=3, border="blue")
+
+# Evaluate Hierarchical clustering performance with k=3
+hierarchical3_vs_true <- table(cuttree3, BLClabels)
+hierarchical3_vs_true
+# BLClabels
+# cuttree3  1  2
+# 1  9  0
+# 2 17  2
+# 3  1 18
+
+# Pretty much the same accuracy as k = 2.
 
 ############################################################################
 #
